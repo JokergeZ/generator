a23f0422d2997eef3de986c2c18ee94ef1ab6151
Rudolf Iohim <r.hahahaha@corp.nstu.ru>
2023-10-23 14:32:41 +0700

    generator1

---
+++ degenerator.py
@@ -0,0 +1,8 @@
+s = [
+    {'id': 1, 'full_name': 'Алекберов Рамиль Русланович'},
+    {'id': 2, 'full_name': 'Бобровская Анастасия Дмитриевна'},
+    {'id': 3, 'full_name': 'Винговатов Александр Олегович'}
+    ]
+
+z = [s['full_name'] for s in s if len(s['full_name'].split())]
+print(z)
